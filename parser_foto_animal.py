9b87aba9dd134d9e55862669a03dfdd626559b54
NadyaEvp <t_evp@mail.ru>
2025-01-25 18:16:06 +0300

    Update parser_foto_animal.py

--- parser_foto_animal.py
+++ parser_foto_animal.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import os
-import json 
+import json
 
 
 def save_json_file(json_data, name_file, folder_static):
@@ -14,10 +14,10 @@ def save_json_file(json_data, name_file, folder_static):
 
 def save_img(urls_img_animal, animal, path):
     paths_img = []
-    name_animal_path = f'{path}/foto_{animal}'
+    name_animal_path = f"{path}/foto_{animal}"
     os.makedirs(name_animal_path, exist_ok=True)
     for i in range(0, len(urls_img_animal)):
-        name_img = f'{name_animal_path}/{animal}_{i+1}.jpg'
+        name_img = f"{name_animal_path}/{animal}_{i+1}.jpg"
         img_data = requests.get(urls_img_animal[i]).content
         with open(name_img, "wb") as handler:
             handler.write(img_data)
@@ -29,17 +29,17 @@ def get_url_img_animal(soup):
     urls_img_animal = []
     tehs_img = soup.find_all("img", class_="Article_image__I_3mF")
     for tech in tehs_img:
-        urls_img_animal.append(tech['data-large-src'])
+        urls_img_animal.append(tech["data-large-src"])
     return urls_img_animal
 
 
 def main():
     try:
-        animal = 'Гепард'
-        folder_static = 'static'
-        name_json_file = f'{animal.lower()}.json'
-        url = f'https://fonwall.ru/search/{animal.lower()}/'
-        params = {'catalog': 'animals'}
+        animal = "Гепард"
+        folder_static = "static"
+        name_json_file = f"{animal.lower()}.json"
+        url = f"https://fonwall.ru/search/{animal.lower()}/"
+        params = {"catalog": "animals"}
         os.makedirs(folder_static, exist_ok=True)
         response = requests.get(url=url, params=params)
         response.raise_for_status()
